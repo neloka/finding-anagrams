d627f4f5b59bb1bcf89e8ca7fb0a651323b6229c
neloka <nelokaiyeke@gmail.com>
2022-06-07 15:41:54 +0100

    finding anagrams push

---
+++ main.py
@@ -0,0 +1,19 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    words = sorted(word)
+    anagrams= sorted(anagram)
+    # words1 = words.replace(' ', '')
+    # anagrams1 = anagrams.replace(' ', '')
+    if words == anagrams:
+        return True
+    else:
+        return False
+
+print(find_anagram('below', 'elbow'))
+print(find_anagram('hello', 'check'))
